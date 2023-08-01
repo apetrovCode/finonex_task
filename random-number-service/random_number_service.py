b3c7adb13081f4431270073afaf4abdcf2272a3c
A Petrov <apetrov.tech@gmail.com>
2023-08-01 11:33:21 +0300

    added random number service

---
+++ random-number-service/random_number_service.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_random_number():
+    return str(random.randint(1, 1000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
