5d227fb2f3b27103fded0ac034171662d0add6bd
A Petrov <apetrov.tech@gmail.com>
2023-08-01 11:37:58 +0300

    initial random name service

--- random-name-service/random_name_service.py
+++ random-name-service/random_name_service.py
@@ -3,9 +3,11 @@ import random
 
 app = Flask(__name__)
 
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
 @app.route('/')
-def get_random_number():
-    return str(random.randint(1, 1000))
+def get_random_name():
+    return random.choice(names)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
